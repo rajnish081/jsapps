f3883e7ccd46a2f087e2fe0ff40a82a6bada4c38
rajnish <kumarrajnish938615@gmail.com>
2025-01-14 19:03:32 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,66 @@
+// let name="raj";
+// let lastName=new String("   kumar  ");
+// console.log(typeof lastName);
+// let sentence='hey i hate you but want to taste you';
+
+// let word=sentence.split(' ');
+// console.log(word);
+
+
+// console.log('this is \' my" goo');
+
+// template literal
+// let message=`this is 
+//    me 
+//       hotty`;
+// console.log(message);  
+
+
+// placeholder
+// let name='ajiti'
+// let message=`hello ${name},
+//     sexy girl
+//     come 
+//       to 
+//       your 
+      
+      
+//       dad`;
+// console.log(message);  
+// console.log(typeof message);    
+
+
+
+// let date =new Date();
+// let date=new Date('june 20 1998 07:15'); 
+// let date=new Date(2003,2,15,6);
+// console.log(date);
+// console.log(typeof date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
